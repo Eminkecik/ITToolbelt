90d77910b7f9bb67c228b1967d37437ef8a3f0c3
Sait ORHAN <saitorhan@outlook.com>
2021-09-06 13:46:32 +0300

    connections

---
+++ ITToolbelt.Entity/Db/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ITToolbelt.Entity.Enum;
+
+namespace ITToolbelt.Entity.Db
+{
+    public class Connection
+    {
+        public int Id { get; set; }
+        public DbServerType DbServerType { get; set; }
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        #region FromServer
+
+        public string MachineName { get; set; }
+        public string ServerName { get; set; }
+        public string Edition { get; set; }
+        public string ProductLevel { get; set; }
+        public string ProductUpdateLevel { get; set; }
+        public string ProductVersion { get; set; }
+        public string Collation { get; set; }
+        public string ProductMajorVersion { get; set; }
+        public string ProductMinorVersion { get; set; }
+
+        #endregion FromServer
+    }
+}
--- ITToolbelt.WinForms/Forms/MainAppForms/FormMain.Designer.cs
+++ ITToolbelt.WinForms/Forms/MainAppForms/FormMain.Designer.cs
@@ -29,13 +29,61 @@ namespace ITToolbelt.WinForms.Forms.MainAppForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dBACockpitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.indexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dBACockpitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1184, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dBACockpitToolStripMenuItem
+            // 
+            this.dBACockpitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.indexToolStripMenuItem});
+            this.dBACockpitToolStripMenuItem.Name = "dBACockpitToolStripMenuItem";
+            this.dBACockpitToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.dBACockpitToolStripMenuItem.Text = "DBA Cockpit";
+            // 
+            // indexToolStripMenuItem
+            // 
+            this.indexToolStripMenuItem.Name = "indexToolStripMenuItem";
+            this.indexToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.indexToolStripMenuItem.Text = "Index";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormMain";
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "IT Toolbelt";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dBACockpitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem indexToolStripMenuItem;
     }
 }
 
