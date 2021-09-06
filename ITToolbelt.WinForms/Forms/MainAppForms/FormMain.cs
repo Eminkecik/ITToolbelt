68375eb4b471be362b74ec6d6bfe24ab6b2fd5a7
Sait ORHAN <saitorhan@outlook.com>
2021-09-06 11:53:30 +0300

    Add project files.

---
+++ ITToolbelt.WinForms/Forms/MainAppForms/FormMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ITToolbelt.WinForms.Forms.MainAppForms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ITToolbelt.WinForms/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace ITToolbelt.WinForms
+{
+    public static class GlobalVariables
+    {
+        public static string DomainName => Environment.UserDomainName;
+        public static string CurrentUser => Environment.UserName;
+        public static string UserWithDomain => $"{DomainName}\\{CurrentUser}";
+        public static string ConnectionString => ConfigurationManager.ConnectionStrings["sql"].ConnectionString;
+    }
+}
