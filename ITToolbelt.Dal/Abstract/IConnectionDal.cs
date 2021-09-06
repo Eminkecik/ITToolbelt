8298eac9fa5b976b74529623ad373691efe03b8a
Sait ORHAN <saitorhan@outlook.com>
2021-09-06 16:12:13 +0300

    IConnectionDal

---
+++ ITToolbelt.Dal/Abstract/IConnectionDal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ITToolbelt.Entity.Db;
+
+namespace ITToolbelt.Dal.Abstract
+{
+    public interface IConnectionDal
+    {
+        bool AddConnection(Connection connection);
+        List<Connection> GetConnections(bool updateFromServer);
+    }
+}
