a0d61d3f320fa38a335cc4aa289d3d2b7012ab3b
Sait ORHAN <saitorhan@outlook.com>
2021-09-07 16:52:15 +0300

    indexes

--- ITToolbelt.WinForms/ExtensionMethods/VisualControls.cs
+++ ITToolbelt.WinForms/ExtensionMethods/VisualControls.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using ITToolbelt.WinForms.InfoForms;
 
 namespace ITToolbelt.WinForms.ExtensionMethods
 {
---
+++ ITToolbelt.WinForms/InfoForms/FormProgress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITToolbelt.WinForms.InfoForms
+{
+    public partial class FormProgress : Form
+    {
+        public FormProgress()
+        {
+            InitializeComponent();
+        }
+    }
+}
