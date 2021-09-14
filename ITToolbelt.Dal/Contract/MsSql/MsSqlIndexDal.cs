6a5db77979552225e3cfba4931febd8e33d774be
Sait ORHAN <saitorhan@outlook.com>
2021-09-14 09:04:45 +0300

    SQL sysntax fixed

--- ITToolbelt.Dal/Contract/MsSql/MsSqlIndexDal.cs
+++ ITToolbelt.Dal/Contract/MsSql/MsSqlIndexDal.cs
@@ -19,7 +19,7 @@ namespace ITToolbelt.Dal.Contract.MsSql
         {
             using (ServerContext serverContext = new ServerContext(ConnectionString))
             {
-                List<Index> indexes = serverContext.Database.SqlQuery<Index>("SELECT S.name as [Schema],T.name as [Table],I.name as [IndexName], [State] = (case I.is_disabled when 1 then 'DISABLED' WHEN 0 THEN 'ENABLED' END), DDIPS.avg_fragmentation_in_percent as Fragmantation,DDIPS.page_count as [PageCount] FROM sys.indexes I join sys.tables T on I.object_id = T.object_id join sys.schemas S on T.schema_id = S.schema_id LEFT join sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) AS DDIPS on DDIPS.object_id = T.object_id AND DDIPS.index_id = I.index_id where i.[name] is not null ORDER BY [Schema], [Table], IndexName").ToList();
+                List<Index> indexes = serverContext.Database.SqlQuery<Index>("SELECT S.name as [Schema],T.name as [Table],I.name as [IndexName], [State] = (case I.is_disabled when 1 then 'DISABLED' WHEN 0 THEN 'ENABLED' END), DDIPS.avg_fragmentation_in_percent as Fragmantation,DDIPS.page_count as [PageCount] FROM sys.indexes I join sys.tables T on I.object_id = T.object_id join sys.schemas S on T.schema_id = S.schema_id LEFT join sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) AS DDIPS on DDIPS.object_id = T.object_id AND DDIPS.index_id = I.index_id where I.[name] is not null ORDER BY [Schema], [Table], IndexName").ToList();
                 return indexes;
             }
         }
