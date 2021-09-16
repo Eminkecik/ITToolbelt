e8c1f8b16c6352e2a48f34a8822d8eb0a81d7683
Sait ORHAN <saitorhan@outlook.com>
2021-09-16 22:30:47 +0300

    mysql login form

---
+++ ITToolbelt.WinForms/Forms/DBAForms/FormMySqlLogin.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace ITToolbelt.WinForms.Forms.DBAForms
+{
+    partial class FormMySqlLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarConnection = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxServerName = new System.Windows.Forms.TextBox();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.textBoxConName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBoxServerInfo = new System.Windows.Forms.GroupBox();
+            this.groupBoxInfo.SuspendLayout();
+            this.groupBoxServerInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(107, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MySQL Server";
+            // 
+            // progressBarConnection
+            // 
+            this.progressBarConnection.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarConnection.Location = new System.Drawing.Point(12, 43);
+            this.progressBarConnection.MarqueeAnimationSpeed = 50;
+            this.progressBarConnection.Name = "progressBarConnection";
+            this.progressBarConnection.Size = new System.Drawing.Size(344, 23);
+            this.progressBarConnection.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarConnection.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Server name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "User name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Password:";
+            // 
+            // textBoxServerName
+            // 
+            this.textBoxServerName.Location = new System.Drawing.Point(107, 19);
+            this.textBoxServerName.Name = "textBoxServerName";
+            this.textBoxServerName.Size = new System.Drawing.Size(230, 20);
+            this.textBoxServerName.TabIndex = 0;
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(107, 45);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(230, 20);
+            this.textBoxUserName.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(107, 72);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(230, 20);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Location = new System.Drawing.Point(281, 252);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelp.TabIndex = 7;
+            this.buttonHelp.Text = "Help";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(200, 252);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(119, 252);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Controls.Add(this.textBoxConName);
+            this.groupBoxInfo.Controls.Add(this.label6);
+            this.groupBoxInfo.Location = new System.Drawing.Point(12, 72);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(344, 53);
+            this.groupBoxInfo.TabIndex = 8;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "Connection Info";
+            // 
+            // textBoxConName
+            // 
+            this.textBoxConName.Location = new System.Drawing.Point(107, 19);
+            this.textBoxConName.Name = "textBoxConName";
+            this.textBoxConName.Size = new System.Drawing.Size(230, 20);
+            this.textBoxConName.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Connection Name:";
+            // 
+            // groupBoxServerInfo
+            // 
+            this.groupBoxServerInfo.Controls.Add(this.label2);
+            this.groupBoxServerInfo.Controls.Add(this.label4);
+            this.groupBoxServerInfo.Controls.Add(this.label5);
+            this.groupBoxServerInfo.Controls.Add(this.textBoxServerName);
+            this.groupBoxServerInfo.Controls.Add(this.textBoxPassword);
+            this.groupBoxServerInfo.Controls.Add(this.textBoxUserName);
+            this.groupBoxServerInfo.Location = new System.Drawing.Point(12, 131);
+            this.groupBoxServerInfo.Name = "groupBoxServerInfo";
+            this.groupBoxServerInfo.Size = new System.Drawing.Size(344, 115);
+            this.groupBoxServerInfo.TabIndex = 9;
+            this.groupBoxServerInfo.TabStop = false;
+            this.groupBoxServerInfo.Text = "Server Info";
+            // 
+            // FormMySqlLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 287);
+            this.Controls.Add(this.groupBoxServerInfo);
+            this.Controls.Add(this.groupBoxInfo);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.progressBarConnection);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMySqlLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Connect to Server";
+            this.groupBoxInfo.ResumeLayout(false);
+            this.groupBoxInfo.PerformLayout();
+            this.groupBoxServerInfo.ResumeLayout(false);
+            this.groupBoxServerInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarConnection;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxServerName;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.GroupBox groupBoxInfo;
+        private System.Windows.Forms.TextBox textBoxConName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBoxServerInfo;
+    }
+}
---
+++ ITToolbelt.WinForms/Forms/DBAForms/FormMySqlLogin.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ITToolbelt.Bll.Managers;
+using ITToolbelt.Entity.Db;
+using ITToolbelt.Entity.Enum;
+using ITToolbelt.WinForms.ExtensionMethods;
+using MySql.Data.MySqlClient;
+
+namespace ITToolbelt.WinForms.Forms.DBAForms
+{
+    public partial class FormMySqlLogin : Form
+    {
+        public bool SuccessFlag { get; set; }
+        public string ConnectionString => sqlConnectionString.ConnectionString;
+        public Connection Connection { get; set; }
+
+        private readonly MySqlConnectionStringBuilder sqlConnectionString;
+        private readonly BackgroundWorker backgroundWorker;
+        private AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        public FormMySqlLogin()
+        {
+            InitializeComponent();
+            sqlConnectionString = new MySqlConnectionStringBuilder();
+
+            Connection = new Connection { DbServerTypeCode = DbServerType.MySql };
+
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBarConnection.StartStopMarque();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            sqlConnectionString.Server = textBoxServerName.Text;
+            sqlConnectionString.UserID = textBoxUserName.Text;
+            sqlConnectionString.Password = textBoxPassword.Text;
+            sqlConnectionString.Password = textBoxPassword.Text;
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectionString.ConnectionString);
+            try
+            {
+                sqlConnection.Open();
+                sqlConnection.Close();
+                SuccessFlag = true;
+
+                Connection.Name = textBoxConName.Text;
+                Connection.ConnectionString = sqlConnectionString.ConnectionString;
+                ConnectionManager connectionManager = new ConnectionManager(GlobalVariables.ConnectionString);
+                SuccessFlag = connectionManager.Add(Connection);
+
+            }
+            catch (Exception exception)
+            {
+                SuccessFlag = false;
+            }
+            finally
+            {
+                if (sqlConnection.State != ConnectionState.Closed)
+                {
+                    sqlConnection.Close();
+                }
+            }
+
+            MessageBox.Show($"Add connection {(SuccessFlag ? "successful" : "failed")}", SuccessFlag ? "Information" : "Error", MessageBoxButtons.OK, SuccessFlag ? MessageBoxIcon.Information : MessageBoxIcon.Error);
+            if (SuccessFlag)
+            {
+                Close();
+            }
+        }
+
+        private void buttonHelp_Click(object sender, System.EventArgs e)
+        {
+            Process.Start("https://dev.mysql.com/doc/connector-net/en/connector-net-connections-string.html");
+        }
+
+        private void buttonCancel_Click(object sender, System.EventArgs e)
+        {
+            SuccessFlag = false;
+            Close();
+        }
+
+        private void buttonConnect_Click(object sender, System.EventArgs e)
+        {
+            progressBarConnection.StartStopMarque();
+            backgroundWorker.RunWorkerAsync();
+        }
+    }
+}
