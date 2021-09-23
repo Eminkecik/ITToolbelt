cbf29afa434eebd2c32519715278ced855c055c4
Sait ORHAN <saitorhan@outlook.com>
2021-09-23 20:31:56 +0300

    user validator

---
+++ ITToolbelt.Bll/Validators/UserValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ITToolbelt.Entity.Db;
+
+namespace ITToolbelt.Bll.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Firstname).NotEmpty().WithMessage("Name field cannot be empty");
+        }
+    }
+}
