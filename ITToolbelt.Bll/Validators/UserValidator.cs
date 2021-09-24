26ab38ce344f32cf55674c7a2e6cbacf6a928e8c
Emin Keçik <eminkecik38@gmail.com>
2021-09-24 09:31:53 +0300

    Update UserValidator.cs

--- ITToolbelt.Bll/Validators/UserValidator.cs
+++ ITToolbelt.Bll/Validators/UserValidator.cs
@@ -8,7 +8,8 @@ namespace ITToolbelt.Bll.Validators
         public UserValidator()
         {
             RuleFor(x => x.Firstname).NotEmpty().WithMessage("Name field cannot be empty");
-            RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname field cannot be empty");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname field cannot be empty");            
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail field cannot be empty");
         }
     }
 }
